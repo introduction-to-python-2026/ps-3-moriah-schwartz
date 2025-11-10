5030e8f1d9e8b549c5512486f1e605375cbac5a2
moriah-schwartz <moriahschwartz12@gmail.com>
2025-11-10 16:56:43 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,14 +1,14 @@
 def move(my_list, direction):
     index_of_one = my_list.index(1)
 
-    if direction == 'right':
+    if direction == 'yemin':
       if index_of_one == len(my_list)-1:
         return my_list
       else:
         my_list[index_of_one] = 0
         my_list[index_of_one + 1] = 1
 
-    elif direction == 'left':
+    elif direction == 'smoll':
       if index_of_one == 0:
         return my_list
       else:
